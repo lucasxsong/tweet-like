0dcc73366633a48ea5d6cd6a9d7c578ffe63dc7c
Lucas Song <lucas7song@gmail.com>
2020-06-03 14:21:34 -0700

    allow users to choose city

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CityDropdown from './citydropdown'
+import CityDropdown from './citydropdown';
 
+interface Props {}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <CityDropdown/>
-      </header>
-    </div>
-  );
+interface State {
+	city: string;
+	showTweets: boolean;
+	cityfile: string;
+}
+
+class App extends React.Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
+		this.state = {
+			city: '',
+			showTweets: false,
+			cityfile: '',
+		};
+	}
+
+	handleSelection = (city: string) => {
+		this.setState({ city: city });
+	};
+
+	showTweets = () => {
+		const fullcity = this.state.city.split(", ");
+		const cityfile = fullcity[0].toLowerCase().replace(/ /g,'');
+		this.setState({ cityfile: cityfile });
+		this.setState({ showTweets: true });
+		console.log(cityfile);
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<h1>
+					Tweet like I'm from {this.state.city ? this.state.city : '...'}
+				</h1>
+				<button onClick={this.showTweets}> GO </button>
+				<div className="selection">
+					<CityDropdown onSelection={this.handleSelection} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
